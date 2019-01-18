70c9e3a014afed7623b2fc02370ddc1d216cfa6e
Joona Kleemola <joona1705@student.hamk.fi>
2019-01-18 10:33:17 +0200

    Testing

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+    firebase: {
+    apiKey: "AIzaSyAatuxoR_ml3_TH4Fyt6ffEsnKgk26RhnQ",
+    authDomain: "databasetest-9e988.firebaseapp.com",
+    databaseURL: "https://databasetest-9e988.firebaseio.com",
+    projectId: "databasetest-9e988",
+    storageBucket: "databasetest-9e988.appspot.com",
+    messagingSenderId: "279965830328"
+  }
 };
