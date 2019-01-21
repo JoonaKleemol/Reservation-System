ca4c0e89ec867f7868856d36905be2a9a93da062
Joona Kleemola <joona1705@student.hamk.fi>
2019-01-21 11:17:34 +0200

    New Io version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Item Reservation';
+  constructor(private router: Router) { }
+  ngOnInit() {
+    this.router.navigate([''])
+  }
 }
